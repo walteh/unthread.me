07b7a994d2add7987cf4d674d2f51fb4e15ebd12
Walter Scott <w@lter.ca>
2024-06-24 14:15:41 -0500

    actually fix routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, HashRouter, Routes, useLocation } from "react-router-dom";
 import { Layout } from "@/app/Layout";
 import { Home, NoMatch } from "@/pages";
 
 import useStore from "../threadsapi/store";
 
 const OAuthCallback: FC = () => {
+	const location = useLocation();
+
 	// Get the token from the URL query parameters
-	const urlParams = new URLSearchParams(window.location.search);
+	const urlParams = new URLSearchParams(location.search);
 	const token = urlParams.get("token");
 
 	if (!token) {
@@ -28,13 +30,15 @@ const OAuthCallback: FC = () => {
 const App: FC = () => {
 	return (
 		<>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route index element={<Home />} />
-					<Route path="*" element={<NoMatch />} />
-					<Route path="auth-redirect" element={<OAuthCallback />} />
-				</Route>
-			</Routes>
+			<HashRouter>
+				<Routes>
+					<Route path="/" element={<Layout />}>
+						<Route index element={<Home />} />
+						<Route path="*" element={<NoMatch />} />
+						<Route path="auth-redirect" element={<OAuthCallback />} />
+					</Route>
+				</Routes>
+			</HashRouter>
 		</>
 	);
 };
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,4 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-// import { Provider } from "react-redux";
-// import { store } from "@/app/store";
 
 import App from "./App.tsx";
 import "./index.css";
@@ -12,10 +9,4 @@ if (!root || !(root instanceof HTMLElement)) {
 	throw new Error("Root element not found");
 }
 
-ReactDOM.createRoot(root).render(
-	<BrowserRouter>
-		{/* <Provider store={store}> */}
-		<App />
-		{/* </Provider> */}
-	</BrowserRouter>,
-);
+ReactDOM.createRoot(root).render(<App />);
