cc07daea3831e523b969bbb38b361d83c71e53ed
Walter Scott <w@lter.ca>
2024-07-13 14:51:29 -0500

    fresh

--- src/components/UserProfile2.tsx
+++ src/components/UserProfile2.tsx
@@ -79,44 +79,45 @@ export default function UserProfile2() {
 	// const item = items.find((item) => item.label === currentTab);
 
 	return (
-		<div className="overflow-hidden rounded-xl bg-white shadow-lg">
-			<h2 className="sr-only" id="profile-overview-title">
-				Profile Overview
-			</h2>
-			<div className="bg-white p-6">
-				<div className="sm:flex sm:items-center sm:justify-between">
-					<div className="sm:flex sm:space-x-5">
-						<div className="flex-shrink-0">
-							<img className="mx-auto h-20 w-20 rounded-full" src={profile?.data?.threads_profile_picture_url} alt="" />
-						</div>
-						<div className="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
-							{/* <p className="text-sm font-medium text-gray-600">Welcome back,</p> */}
-							<p className="text-3xl font-bold font-rounded text-gray-900 sm:text-3xl">@{profile?.data?.username}</p>
-							<p className="text-lg font-medium text-gray-600">{profile?.data?.threads_biography ?? "..."}</p>
-						</div>
-					</div>
-					<div className="mt-5 flex justify-around sm:mt-0">
-						<button
-							onClick={refresh}
-							className="flex  items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-						>
-							<div className="flex flex-col">
-								<span>🔄</span>
-								<span className="">cache</span>
+		<div>
+			<div className="rounded-xl bg-white shadow-lg">
+				<h2 className="sr-only" id="profile-overview-title">
+					Profile Overview
+				</h2>
+				<div className="p-6">
+					<div className="sm:flex sm:items-center sm:justify-between">
+						<div className="sm:flex sm:space-x-5">
+							<div className="flex-shrink-0">
+								<img className="mx-auto h-20 w-20 rounded-full" src={profile?.data?.threads_profile_picture_url} alt="" />
 							</div>
-							<div className=" flex flex-col items-start justify-center  rounded-md  text-xs w-full mt-2">
-								{loaders.map((stat) => (
-									<span key={stat.label} className="ml-3 flex text-xs mb-2">
-										{stat.data?.is_loading ? <Loader /> : "✅"} {stat.label}
-									</span>
-								))}
+							<div className="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
+								{/* <p className="text-sm font-medium text-gray-600">Welcome back,</p> */}
+								<p className="text-3xl font-bold font-rounded text-gray-900 sm:text-3xl">@{profile?.data?.username}</p>
+								<p className="text-lg font-medium text-gray-600">{profile?.data?.threads_biography ?? "..."}</p>
 							</div>
-						</button>
+						</div>
+						<div className="mt-5 flex justify-around sm:mt-0">
+							<button
+								onClick={refresh}
+								className="flex  items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+							>
+								<div className="flex flex-col">
+									<span>🔄</span>
+									<span className="">cache</span>
+								</div>
+								<div className=" flex flex-col items-start justify-center  rounded-md  text-xs w-full mt-2">
+									{loaders.map((stat) => (
+										<span key={stat.label} className="ml-3 flex text-xs mb-2">
+											{stat.data?.is_loading ? <Loader /> : "✅"} {stat.label}
+										</span>
+									))}
+								</div>
+							</button>
+						</div>
 					</div>
 				</div>
-			</div>
 
-			{/* <div
+				{/* <div
 				className={`grid grid-cols-1 divide-y divide-gray-200 border-t border-gray-200 bg-gray-50 lg:grid-cols-4 sm:divide-x sm:divide-y-0`}
 			>
 				{loaders.map((stat) => (
@@ -126,34 +127,35 @@ export default function UserProfile2() {
 				))}
 			</div> */}
 
-			<div
-				className={`grid grid-cols-1 divide-y divide-gray-200 border-t border-gray-200 bg-gray-50 lg:grid-cols-${stats.length} sm:divide-x sm:divide-y-0`}
-			>
-				{stats.map((stat) => (
-					<div key={stat.label} className="px-6 py-5 text-center text-sm font-medium">
-						<span className="text-gray-900">{stat.value}</span> <span className="text-gray-600">{stat.label}</span>
-					</div>
-				))}
-			</div>
+				<div
+					className={`grid grid-cols-2 divide-y divide-gray-200 border-t border-gray-200 bg-gray-50 lg:grid-cols-${stats.length} sm:divide-x sm:divide-y-0`}
+				>
+					{stats.map((stat) => (
+						<div key={stat.label} className="px-6 py-5 text-center text-sm font-medium">
+							<span className="text-gray-900">{stat.value}</span> <span className="text-gray-600">{stat.label}</span>
+						</div>
+					))}
+				</div>
 
-			<div
-				className={`grid grid-cols-1 divide-y divide-gray-200 border-t border-gray-200 bg-gray-50 lg:grid-cols-${items.length} sm:divide-x sm:divide-y-0`}
-			>
-				{items.map((tab) => (
-					<button
-						key={tab.label}
-						onClick={() => {
-							setCurrentTab(tab.comp());
-							setOpenModal(true);
-						}}
-						className="px-6 py-5 text-center text-sm font-medium bg-white hover:bg-gray-100 border border-gray-200 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-					>
-						{tab.label}
-					</button>
-				))}
-			</div>
+				<div
+					className={`grid grid-cols-2 divide-y divide-gray-200 border-t border-gray-200 bg-gray-50 lg:grid-cols-${items.length} sm:divide-x sm:divide-y-0 rounded-xl shadow-lg`}
+				>
+					{items.map((tab) => (
+						<button
+							key={tab.label}
+							onClick={() => {
+								setCurrentTab(tab.comp());
+								setOpenModal(true);
+							}}
+							className="px-6 py-5 text-center text-sm font-medium bg-white hover:bg-gray-100 border border-gray-200 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+						>
+							{tab.label}
+						</button>
+					))}
+				</div>
 
-			<Modal>{currentTab}</Modal>
+				<Modal>{currentTab}</Modal>
+			</div>
 		</div>
 	);
 }
