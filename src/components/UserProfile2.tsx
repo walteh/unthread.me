ba7352ae3b1eb3bca47c72b4221fb58e95e4b6ff
Walter Scott <w@lter.ca>
2024-07-21 16:11:40 -0500

    fix styles

--- src/components/UserProfile2.tsx
+++ src/components/UserProfile2.tsx
@@ -17,7 +17,7 @@ import UserThreadsView from "./UserThreadsView";
 import WordSegmentLineChart from "./WordSegmentLineChart";
 
 const Loader = () => (
-	<div className="flex justify-center items-center h-full">
+	<div className="flex justify-center items-center">
 		<div className="loader ease-linear rounded-full border-2 border-t-2 border-gray-200 h-4 w-4"></div>
 	</div>
 );
