18a926afbd7bc7100a066a229b88bd668bc6a0aa
Walter Scott <w@lter.ca>
2024-07-22 10:29:50 -0500

    add correct login scope

--- src/threadsapi/generate_auth_start_url.ts
+++ src/threadsapi/generate_auth_start_url.ts
@@ -10,7 +10,7 @@ export const generate_auth_start_url = (state?: string): URL => {
 	const authUrl = new URL("https://threads.net/oauth/authorize");
 	authUrl.searchParams.append("client_id", client_id);
 	authUrl.searchParams.append("redirect_uri", current_host);
-	authUrl.searchParams.append("scope", "threads_basic");
+	authUrl.searchParams.append("scope", "threads_basic,threads_read_replies,threads_manage_insights");
 	authUrl.searchParams.append("response_type", "code");
 
 	if (state) {
